b30272ec6bf758db7ee165b387ec27886e2a2206
N-sy <67050028+N-sy@users.noreply.github.com>
2026-01-13 12:51:25 +0530

    feat: Upgrade Angular and Vite versions
    Updates Angular dependencies to v19, Vite to v6, and TypeScript to v5.6.2. This upgrade also involves updating the tsconfig resolution in `vite.config.ts` and adjusting CDN imports in `index.html`.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-
 import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   return {
     plugins: [
       angular({
-        tsconfig: './tsconfig.app.json',
+        tsconfig: path.resolve(__dirname, 'tsconfig.app.json'),
       }),
     ],
     define: {
